b2d028f3e6f408d86a0911e91a26c42df0bd3b8f
Harshvardhan <srivastavahv@gmail.com>
2022-02-09 23:22:54 +0530

    added confidence score

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import os
 from sklearn.model_selection._split import StratifiedKFold
 from models import ModelSelection 
 from sklearn.metrics import f1_score,classification_report,confusion_matrix
-
+from collections import Counter
 
 ############### Preparing Data ##################
 dataset = PathologicalGamblingDataset(os.getcwd())
 trn_data,trn_cat= dataset.get_data()
 
 ############### Debugging on small dataset ###### 
-trn_data,trn_cat = trn_data[:500],trn_cat[:500]
+# trn_data,trn_cat = trn_data[:50],trn_cat[:50]
 
 ############### Choosing Model and Model Parameters ##################
 option = 'entropy'
@@ -21,7 +21,7 @@ model = ModelSelection(option,clf_opt,num_features)
 ############### KFold Cross Validation ##########
 skf = StratifiedKFold(n_splits=10)
 
-predicted_class_labels=[]; actual_class_labels=[]; count=0;
+predicted_class_labels=[]; actual_class_labels=[]; count=0; confidence_score = []
 for train_index, test_index in skf.split(trn_data,trn_cat):
     X_train=[]; y_train=[]; X_test=[]; y_test=[]
     for item in train_index:
@@ -34,19 +34,19 @@ for train_index, test_index in skf.split(trn_data,trn_cat):
     print('\n')                
     print('CV Level '+str(count))
     predicted,predicted_probability= model.fit(X_train,y_train,X_test)
+    confidence_score.append(predicted_probability)
     for item in y_test:
         actual_class_labels.append(item)
     for item in predicted:
         predicted_class_labels.append(item)
 
+#   Evaluation 
+class_names = list(Counter(actual_class_labels).keys())
 
-fm=f1_score(actual_class_labels, predicted_class_labels, average='macro') 
-print ('\n Macro Averaged F1-Score :'+str(fm))
-fm=f1_score(actual_class_labels, predicted_class_labels, average='micro') 
-print ('\n Micro Averaged F1-Score:'+str(fm))
 
 print(classification_report(actual_class_labels,predicted_class_labels))
-
 tn, fp, fn, tp = confusion_matrix(actual_class_labels, predicted_class_labels).ravel()
 specificity = tn / (tn+fp)
 print('\n Specifity Score :',str(specificity))
+print ('The Probablity of Confidence of the Classifier: \t'+str(sum(confidence_score)/len(confidence_score))+'\n')
+
