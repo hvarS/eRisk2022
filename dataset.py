a7fc9a25318b7433116e6f9a57193cfd0d66b03b
Harshvardhan <srivastavahv@gmail.com>
2022-02-09 16:12:29 +0530

    added main file to use

--- dataset.py
+++ dataset.py
@@ -2,6 +2,8 @@ from torch.utils.data import Dataset
 import os 
 import xml.etree.ElementTree as ET
 import re
+from tqdm import tqdm
+
 
 task1_loc = 'task1_data'
 task1_training_loc = 't1_training/TRAINING_DATA/2021_cases'
@@ -11,19 +13,16 @@ class PathologicalGamblingDataset(Dataset):
         super(PathologicalGamblingDataset,self).__init__()
         self.path = path
         trn_data,trn_cat=self.get_training_data() 
-        print(type(trn_data[0]))
 
     def get_training_data(self):
-        print('\n ***** Getting Training Data ***** \n')
+        print('\n ***** Reading Training Data ***** \n')
 
         training_loc = os.path.join(self.path,task1_loc,task1_training_loc)
         golden_truth_path = os.path.join(self.path,task1_loc,task1_training_loc,'risk_golden_truth.txt')
         
         fl=open(golden_truth_path, 'r')  
         reader = fl.readlines()
-        
         fl.close()
-        
         golden_truths={}; unique_id=[]
         for item in reader:
             idn=item.split(' ')[0]
@@ -36,7 +35,7 @@ class PathologicalGamblingDataset(Dataset):
         trn_data=[]; trn_cat=[];  trn_dict={}
         trn_files=os.listdir(os.path.join(training_loc,'data'))
 
-        for file in trn_files:
+        for file in tqdm(trn_files):
             if file.find('.xml')>0:
 #                print('Processing Training File: '+file)
                 tree = ET.parse(os.path.join(training_loc,'data',file))
