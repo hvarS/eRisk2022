9d113d8eef7d5cab533471c1cd2c0513efc9db3f
Harshvardhan <srivastavahv@gmail.com>
2022-02-09 17:28:22 +0530

    added doc2vec baseline

--- baselines.py
+++ baselines.py
@@ -14,7 +14,7 @@ path = os.getcwd()
 
 if not os.path.exists(os.path.join(path,'saved_models')):
     os.mkdir(os.path.join(path,'saved_models'))
-    
+
 
 # TFIDF model    
 def tfidf_training_model(self,trn_data,trn_cat,no_of_selected_features = None,clf_opt = 'ab'):
@@ -96,3 +96,42 @@ def entropy_training_model(trn_data,trn_cat,no_of_selected_features = 1000,clf_o
     joblib.dump(trn_dct, flname+'_dict.joblib')
     
     return clf,ext2,trn_dct,trn_model
+
+from gensim.models.doc2vec import Doc2Vec,TaggedDocument 
+
+# Doc2Vec model    
+def doc2vec_training_model(self,trn_data,trn_cat,no_of_selected_features = 1000,clf_opt = 'ab'):
+    print('\n ***** Building Doc2Vec Based Training Model ***** \n')
+    print('No of Features \t'+str(no_of_selected_features)) 
+    tagged_data = [TaggedDocument(words=nltk.word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(trn_data)]
+    max_epochs = 10       
+    trn_model = Doc2Vec(vector_size=no_of_selected_features,alpha=0.025,min_alpha=0.00025,min_count=1,dm =1)
+    trn_model.build_vocab(tagged_data)  
+    print('Number of Training Samples {0}'.format(trn_model.corpus_count))   
+    for epoch in range(max_epochs):
+        print('Doc2Vec Iteration {0}'.format(epoch))
+        trn_model.train(tagged_data,
+                    total_examples=trn_model.corpus_count,
+                    epochs=100) 
+        # decrease the learning rate
+        trn_model.alpha -= 0.0002
+    trn_vec=[]
+    for i in range(0,len(trn_data)):
+            vec=[] 
+            for v in trn_model.docvecs[i]:
+                vec.append(v)
+            trn_vec.append(vec)
+# Classificiation and feature selection pipelines
+    clf,clf_parameters,ext2=classification_pipeline(clf_opt) 
+    pipeline = Pipeline([('clf', clf),])       
+    grid = GridSearchCV(pipeline,clf_parameters,scoring='f1_micro',cv=10) 
+    grid.fit(trn_vec,trn_cat)     
+    clf= grid.best_estimator_
+
+    model_path = os.path.join('saved_models','doc2vec_'+clf_opt)
+    flname=model_path+'doc2vec'+'_'+clf_opt+'_'+str(no_of_selected_features)
+    
+    joblib.dump(clf, flname+'_clf.joblib')
+    joblib.dump(trn_model, flname+'_model.joblib')
+            
+    return clf,ext2,trn_model
