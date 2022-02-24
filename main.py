b934a94cee768d84f2578c8cf442d2a997f3ccab
Harshvardhan <srivastavahv@gmail.com>
2022-02-24 09:16:40 +0530

    added jobs in doc2vec

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ dataset = PathologicalGamblingDataset(os.path.join(os.getcwd(),args.train_loc),a
 trn_data,trn_cat= dataset.get_data()
 
 ############### Debugging on small dataset ###### 
-trn_data,trn_cat = trn_data[:200],trn_cat[:200]
+# trn_data,trn_cat = trn_data[:500],trn_cat[:500]
 
 ############### Choosing Model and Model Parameters ##################
 option = args.model
